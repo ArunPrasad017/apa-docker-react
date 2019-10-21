d55d0b99fab96c17c2d346f72029e047068781fb
aralagarsamy <aralagarsamy@egencia.com>
2019-10-21 15:56:44 -0700

    modifying file app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I had to change it now to test
+          I had to change it now to test and thats it
         </p>
         <a
           className="App-link"
